9e5979aeffa4c7c2a4d0169fbcdf0aeffa518c32
AlbertAgzamov <agzamov.albert.1998@gmail.com>
2024-02-01 15:49:50 +0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -17,12 +17,27 @@ def get_requirements(file_path: str) -> List[str]:
     return requirements
 
 
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+__version__ = '0.0.0'
+
+REPO_NAME = 'mushroom-classifier'
+AUTHOR_USER_NAME = 'AlbertAgzamov'
+SRC_REPO = 'MushroomBinaryClassifier'
+AUTHOR_EMAIL = 'agzamov.albert.1998@gmail.com'
+
 setup(
-    name='MushroomBinaryClassifier',
-    version='0.0.1',
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
     description='A Python package for binary classification of mushrooms, helping to determine their edibility.',
-    author='Albert Agzamov',
-    author_email='agzamov.albert.1998@gmail.com',
+    long_description=long_description,
+    long_description_content='text/markdown',
+    url=f'https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}',
     packages=find_packages(),
+    # package_dir={'': 'src'}
     install_requires=get_requirements('requirements.txt')
 )
