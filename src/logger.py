911908ab9caffa48496c25a97d842f69f0aacfcf
AlbertAgzamov <agzamov.albert.1998@gmail.com>
2024-02-08 12:54:33 +0400

    Fix src/logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,8 +1,8 @@
 import logging
 import os
-from datetime import datetime
+import datetime
 
-LOG_FILE_NAME = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE_NAME = f"{datetime.datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join('logs', LOG_FILE_NAME)
 os.makedirs(logs_path, exist_ok=True)
 
@@ -10,10 +10,10 @@ LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE_NAME)
 
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH, 
-    encoding='utf-8', 
+    filename=LOG_FILE_PATH,
+    encoding='utf-8',
     level=logging.INFO,
-    format='%(asctime)s - %(levelname)s %(lineno)s - %(message)s',
+    format='%(asctime)s - %(levelname)s - %(lineno)s - %(message)s',
     filemode='w'
     )
 
